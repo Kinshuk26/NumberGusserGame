866e23e4e92162252a1a8b279cc482b4948d0635
Kinshuk26 <147161528+Kinshuk26@users.noreply.github.com>
2025-08-24 14:48:23 +0530

    NumberGuessingGame.py

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -2,14 +2,14 @@ import random
 
 def play_game():                       # Defining a function here!
     print("Welcome to the Number Guessing Game!")
-    print("I have chosen a number between 1 and 100.")
-    print("Try to guess it!\n")
+    print("Try to guess a number between 1 to 100.")
+    print("All the best! \n")
 
     number = random.randint(1, 100)    # Computer choosing a random number!
     attempts = 0
 
     while True:                        # Starting a while loop!
-        guess = int(input("Enter your guess: "))
+        guess = int(input("Enter your number: "))
         attempts += 1
 
         if guess < number:
@@ -17,12 +17,13 @@ def play_game():                       # Defining a function here!
         elif guess > number:
             print("Too High!")
         else:
-            print("Correct! You guessed it in", attempts, "attempts.")
+            print("Correct! You guessed it in", attempts, "attempts.")     # Breaking the loop once the number is guessed!
             break
 
 while True:
     play_game()
-    again = input("Do you want to play again? (yes/no): ")  # Confirming if the player wants to play again! If yes then reruning the loop else breaking the loop!
+    again = input("Do you want to play again? (yes/no): ")  # Confirming if the player wants to play again! If yes, then rerunning the loop, else breaking the loop!
     if again.lower() != "yes":
         print("Thanks for playing!")
         break
+
